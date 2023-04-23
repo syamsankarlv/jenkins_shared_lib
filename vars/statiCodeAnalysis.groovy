0cc63b536224e4916176a86bcba704b17eef04dc
syamsankarlv <sankarlvsyam@gmail.com>
2023-04-23 19:00:07 +0530

    static-new2

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,10 +1,7 @@
 def call(credentialsId){
    
   withSonarQubeEnv(credentialsId: credentialsId) {
-     sh 'mvn clean package sonar:sonar \
-  -Dsonar.projectKey=Java-project \
-  -Dsonar.host.url=http://54.173.70.205:9000 \
-  -Dsonar.login=squ_1b8038d874ee416167d6f185410415112b02ba5d'
+     sh 'mvn clean verify sonar:sonar -Dsonar.login=squ_1b8038d874ee416167d6f185410415112b02ba5d'
   }
 
 }
